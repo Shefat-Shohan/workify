b104e7dee803ab014af208a1ded0c2bb3b6d33e7
Shefat-Shohan <shohan.ks3@gmail.com>
2024-08-29 03:33:41 +0600

    fix listing

--- components/JobListing.tsx
+++ components/JobListing.tsx
@@ -26,7 +26,7 @@ export default function JobListing({
                 </div>
                 <div className="text-gray-400 flex items-center">
                   <MapPinIcon className="w-5 h-5 text-gray-400  mr-1" />
-                  {job.location}
+                  {job?.location}
                 </div>
               </div>
               <div className="pb-4">
