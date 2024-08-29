35918d726c46391a3b5252e531c59d6baf8a462f
Shefat-Shohan <shohan.ks3@gmail.com>
2024-08-29 17:09:43 +0600

    search list fix

--- components/search/SearchResultList.tsx
+++ components/search/SearchResultList.tsx
@@ -2,21 +2,25 @@ import React, { useRef, useEffect, useState } from "react";
 import SearchResult from "./SearchResult";
 import { jobdataType } from "../JobListings";
 
-export default function SearchResultList({ results }) {
-
+export default function SearchResultList({
+  results,
+}: {
+  results: jobdataType[];
+}) {
+  console.log("results", results);
   return (
     <div>
-        <div className="relative">
-          {results.length > 0 && (
-            <div
-              className={` w-full bg-black-100 border border-b-0 border-gray-50/15 flex flex-col shadow-md rounded-lg  max-h-[300px] overflow-y-scroll mt-4 absolute top-0 z-[100]`}
-            >
-              {results.map((result:jobdataType) => (
-                <SearchResult key={result.id} result={result} />
-              ))}
-            </div>
-          )}
-        </div>
+      <div className="relative">
+        {results.length > 0 && (
+          <div
+            className={` w-full bg-black-100 border border-b-0 border-gray-50/15 flex flex-col shadow-md rounded-lg  max-h-[300px] overflow-y-scroll mt-4 absolute top-0 z-[100]`}
+          >
+            {results.map((result: jobdataType) => (
+              <SearchResult key={result.id} result={result} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
