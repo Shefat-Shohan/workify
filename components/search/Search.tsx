e4b65363ab85e276c9e710296a71933dbe0d9f2e
Shefat-Shohan <shohan.ks3@gmail.com>
2024-08-29 17:05:38 +0600

    search type fix

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -2,13 +2,16 @@
 import { useState } from "react";
 import SearchBar from "./SearchBar";
 import SearchResultList from "./SearchResultList";
+import { jobdataType } from "../JobListings";
 
 export default function Search() {
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<React.SetStateAction<jobdataType[]>>(
+    []
+  );
   return (
     <div className="w-full lg:w-[60%] h-full">
       <SearchBar setResults={setResults} />
-       <SearchResultList results={results} />
+      <SearchResultList results={results} />
     </div>
   );
 }
